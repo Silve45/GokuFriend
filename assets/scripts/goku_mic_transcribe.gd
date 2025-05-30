13a2228ea148771d4dd036012e5d81af9ea88a12
Silve45 <achijioke03@gmail.com>
2025-05-30 11:56:27 -0400

    initial commit -ignoring addons because I couldn't publish to github! Just be sure to readd them!

---
+++ assets/scripts/goku_mic_transcribe.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@export var chat : NobodyWhoChat
+@export var myWords : RichTextLabel
+@onready var mic_player: AudioStreamPlayer = $CaptureStreamToText/MicPlayer
+
+func _ready() -> void:
+	chat.system_prompt = "your name is goku and you are a saiyan from earth! NEVER USE EMOJIS IN YOUR REPONSIVES"
+	chat.start_worker()
+	
+
+var canRecord = false
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("enter"):
+		printerr("enter pressed")
+		_trial_run()
+	if event.is_action_pressed("debug"):
+		canRecord = !canRecord
+		$CaptureStreamToText/Panel/Label.text = ""
+		_can_play()
+
+func _can_play():
+	if canRecord == true:
+		#$CaptureStreamToText.recording = true
+		mic_player.play()
+	else:
+		mic_player.stop()
+		#$CaptureStreamToText.recording = false
+
+
+func _trial_run():
+	
+	chat.say("what is your name then?")
+	
+	var response = await chat.response_finished
+	print("He said: " + response)
+	var voices = DisplayServer.tts_get_voices_for_language("en")
+	var voice_id = voices[0]
+	
+	DisplayServer.tts_speak(response, voice_id)
+	
