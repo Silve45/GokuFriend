439facb7db66e6fe73a44a400837eeec0fbf928c
Silve45 <achijioke03@gmail.com>
2025-06-01 16:15:15 -0400

    continued work on goku -made mouth animations and a way to move and stop the mouth (using TTS callback)

--- assets/scripts/goku_mic_transcribe.gd
+++ assets/scripts/goku_mic_transcribe.gd
@@ -4,21 +4,39 @@ extends Control
 @export var myWords : RichTextLabel
 @onready var mic_player: AudioStreamPlayer = $CaptureStreamToText/MicPlayer
 @export var captureStreamToText : CaptureStreamToText
+var regularMouth = load("res://assets/blender/goku/imagesAndPSDS/mouth.png")
+var talkingMouth1 = load("res://assets/blender/goku/imagesAndPSDS/mouthTalking1.png")
+var canRecord = false
+
 
 func _ready() -> void:
 	chat.system_prompt = "your name is goku and you are a saiyan from earth! NEVER USE EMOJIS IN YOUR REPONSIVES"
 	chat.start_worker()
 	
 
-var canRecord = false
+func _mouth_animation_start():
+	$"3dScene/GokuScene/mouthAnimations".play("mouthAnimation")
+	#await DisplayServer.tts_is_speaking() == false
+	#print("stop")
+	#_mouth_animation_stop()
+	
+
+func _mouth_animation_stop():
+	$"3dScene/GokuScene/mouthAnimations".play("idleMouth")
+
+
+
+var incrementNum = 0
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("enter"):
 		#_trial_run()
-		_start_thread()
+		#_start_thread()
+		pass
 	if event.is_action_pressed("debug"):
-		canRecord = !canRecord
-		$CaptureStreamToText/Panel/Label.text = ""
-		_can_play()
+		_trial_run()
+		#canRecord = !canRecord
+		#$CaptureStreamToText/Panel/Label.text = ""
+		#_can_play()
 
 func _can_play():
 	if canRecord == true:
@@ -54,15 +72,36 @@ func _trial_run():
 	var words = myWords.text
 	var finalWords = _replace_first(words, extraWords, "")
 	
-	print("I said : ", finalWords)
-	chat.say(finalWords)
+	var testWords = "what is your name"
+	
+	print("I said : ", testWords)
+	chat.say(testWords)
 	
 	extraWords = words #add all previous words to this string
-	
 	var response = await chat.response_finished
+	
 	print("He said: " + response)
 	var voices = DisplayServer.tts_get_voices_for_language("en")
 	var voice_id = voices[0]
 	
+	DisplayServer.tts_set_utterance_callback(DisplayServer.TTS_UTTERANCE_ENDED, _stop_mouth)
 	DisplayServer.tts_speak(response, voice_id)
+	_mouth_animation_start()
 	
+
+
+func _stop_mouth(_id):
+	_mouth_animation_stop()
+	printerr(" DONENOEONENOE")
+
+
+func _mouth_animation(whichMouth : int):
+	var pickedMouth : Texture
+	match whichMouth:
+		0:
+			pickedMouth = regularMouth
+		1:
+			pickedMouth = talkingMouth1
+
+	var mouthMaterial = $"3dScene/GokuScene/goku/headShape".get_active_material(2)
+	mouthMaterial.albedo_texture =  pickedMouth
