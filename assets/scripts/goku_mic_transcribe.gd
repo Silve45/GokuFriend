89a87fa9031a6e91e92393c7ef0b02fc502f420d
Silve45 <achijioke03@gmail.com>
2025-06-01 20:25:23 -0400

    alright it works! - need to see why the audio_ctx keeps getting to big though... but besides that YEAH! --extra notes (I typed some in the capture stream to text gd, but I realized that it is ignored... may need to move it out or something!

--- assets/scripts/goku_mic_transcribe.gd
+++ assets/scripts/goku_mic_transcribe.gd
@@ -8,41 +8,108 @@ extends Control
 @onready var headAnimationTree = $"3dScene/GokuScene/headAnimations/headAnimationTree"
 var regularMouth = load("res://assets/blender/goku/imagesAndPSDS/mouth.png")
 var talkingMouth1 = load("res://assets/blender/goku/imagesAndPSDS/mouthTalking1.png")
-var canRecord = false
+
 
 
 func _ready() -> void:
 	chat.system_prompt = "your name is goku and you are a saiyan from earth! NEVER USE EMOJIS IN YOUR REPONSIVES"
 	chat.start_worker()
-	
+
 
 func _mouth_animation_start():
 	mouthAnimationTree.set("parameters/conditions/moving",true)
 	mouthAnimationTree.set("parameters/conditions/idle",false)
-	headAnimationTree.set("parameters/conditions/moving", true)
-	headAnimationTree.set("parameters/conditions/idle", false)
+
 
 func _mouth_animation_stop():
 	mouthAnimationTree.set("parameters/conditions/idle",true)
 	mouthAnimationTree.set("parameters/conditions/moving",false)
-	headAnimationTree.set("parameters/conditions/idle", true)
-	headAnimationTree.set("parameters/conditions/moving", false)
+	#going back to idle state as well
+	_trigger_head_animation_state(0)#idle
+	$CaptureStreamToText/Panel/Label._wipe_text()
+	isTalking = false
+
+func _trigger_head_animation_state(state):
+	headAnimationTree.set("parameters/conditions/idle", false)
+	headAnimationTree.set("parameters/conditions/listening", false)
+	headAnimationTree.set("parameters/conditions/talking", false)
+	match state:
+		0, "idle":
+			headAnimationTree.set("parameters/conditions/idle", true)
+		1, "listening", "listen":
+			headAnimationTree.set("parameters/conditions/listening", true)
+		2, "talking", "talk":
+			headAnimationTree.set("parameters/conditions/talking", true)
 
 
-
-var incrementNum = 0
+var started = false
+var canRecord = false
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("enter"):
-		pass
-		#_trial_run()
-		#_start_thread()
+		if started == false:
+			_start_thread()
+			started = true
 		
-	if event.is_action_pressed("debug"):
-		_trial_run()
-		#canRecord = !canRecord
-		#$CaptureStreamToText/Panel/Label.text = ""
+	if event.is_action_pressed("space"):
+		#_trial_run()
+		if isTalking == false:
+			canRecord = !canRecord
+			if canRecord == true:
+				_listening()
+			else:
+				_talking()
+			
+		#$CaptureStreamToText/Panel/Label.text = "" #sa does nothing
 		#_can_play()
 
+
+func _listening():
+	mic_player.play()
+	_trigger_head_animation_state(1)#listening
+	$CaptureStreamToText/Panel/Label._restart_text()#starts text if available
+
+var isTalking = false
+func _talking():
+	isTalking = true
+	#stop lisenting and switch animations
+	mic_player.stop()
+	_trigger_head_animation_state(2)#talking
+	
+	var words = myWords.get_parsed_text()
+	#var words = "what is your name" #this is a test
+	
+	print("I said : ", words)
+	chat.say(words)
+	
+	var response = await chat.response_finished
+	
+	print("He said: " + response)
+	var voices = DisplayServer.tts_get_voices_for_language("en")
+	var voice_id = voices[0]
+	
+	DisplayServer.tts_set_utterance_callback(DisplayServer.TTS_UTTERANCE_ENDED, _stop_mouth)
+	DisplayServer.tts_speak(response, voice_id)
+	_mouth_animation_start()
+
+
+func _stop_mouth(_id):
+	_mouth_animation_stop()
+	
+
+
+func _mouth_animation(whichMouth : int):
+	var pickedMouth : Texture
+	match whichMouth:
+		0:
+			pickedMouth = regularMouth
+		1:
+			pickedMouth = talkingMouth1
+
+	var mouthMaterial = $"3dScene/GokuScene/goku/headShape".get_active_material(2)
+	mouthMaterial.albedo_texture =  pickedMouth
+
+
+#depreciated can_play
 func _can_play():
 	if canRecord == true:
 		#$CaptureStreamToText.recording = true
@@ -70,42 +137,3 @@ func _replace_first(from: String, what: String, forwhat: String):
 	var idx = from.find(what)
 	if idx == -1: return from
 	return from.substr(0, idx) + forwhat + from.substr(idx + what.length())
-
-
-var extraWords : String
-func _trial_run():
-	var words = myWords.text
-	var finalWords = _replace_first(words, extraWords, "")
-	
-	var testWords = "what is your name"
-	
-	print("I said : ", testWords)
-	chat.say(testWords)
-	
-	extraWords = words #add all previous words to this string
-	var response = await chat.response_finished
-	
-	print("He said: " + response)
-	var voices = DisplayServer.tts_get_voices_for_language("en")
-	var voice_id = voices[0]
-	
-	DisplayServer.tts_set_utterance_callback(DisplayServer.TTS_UTTERANCE_ENDED, _stop_mouth)
-	DisplayServer.tts_speak(response, voice_id)
-	_mouth_animation_start()
-	
-
-
-func _stop_mouth(_id):
-	_mouth_animation_stop()
-
-
-func _mouth_animation(whichMouth : int):
-	var pickedMouth : Texture
-	match whichMouth:
-		0:
-			pickedMouth = regularMouth
-		1:
-			pickedMouth = talkingMouth1
-
-	var mouthMaterial = $"3dScene/GokuScene/goku/headShape".get_active_material(2)
-	mouthMaterial.albedo_texture =  pickedMouth
